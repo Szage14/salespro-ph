5460cf5fb040d5acc1b1bc23b9548e272f445115
Szage14 <cristianjay.buquis@carsu.edu.ph>
2026-06-02 20:14:25 +0800

    Add Vue app and fix Vuetify import

---
+++ salespro-app/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-main class="pa-4">
+      <v-card>
+        <v-card-title>SalesPro PH</v-card-title>
+        <v-card-text>
+          Vuetify is working 🚀
+        </v-card-text>
+      </v-card>
+    </v-main>
+  </v-app>
+</template>
